c875d64da401c8ef4fb1166d4b2e413b5ea592bd
kbeduhn <kasey.beduhn@gmail.com>
2026-05-28 12:15:18 -0500

    Add modules for static site generator

---
+++ htmlnode.py
@@ -0,0 +1,55 @@
+class HTMLNode():
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError("to_html not implemented")
+
+    def props_to_html(self):
+        result = ""
+        if self.props == None or self.props == {}:
+            return ""
+        else:
+            for prop in self.props:
+                result+=f' {prop}="{self.props[prop]}"'
+        return result
+
+    def __repr__(self):
+        return f"HTMLNode: tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props}"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError (f"All leaf nodes must have a value")
+        elif self.tag is None:
+            return self.value
+        else:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"HTMLNode: tag: {self.tag}, value: {self.value}, props: {self.props}"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, value=None, children=children, props=props)
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError (f"Object must have a tag")
+        elif self.children is None:
+            raise ValueError (f"Children must have a value")
+        else:
+            result = f"<{self.tag}{self.props_to_html()}>"
+            for child in self.children:
+                result += child.to_html()
+            result += f"</{self.tag}>"
+        return result
+
+
